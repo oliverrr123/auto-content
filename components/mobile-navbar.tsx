4d2756fc277ce4a2d1c1f6c1b024730e6e6185a2
oliverrr123 <oliver.cingl@gmail.com>
2025-05-23 14:55:09 +0200

    feat: add mobile navbar and header

---
+++ components/mobile-navbar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function MobileNavbar() {
+    return (
+        <div className="fixed flex justify-between items-center w-full h-20 px-10 bottom-0 left-0 right-0 bg-white drop-shadow-sexy">
+            <Link href="/">
+                <Image src="/icons/navbar/home.svg" alt="Home" width={24} height={24} />
+            </Link>
+            <Link href="/scheduling">
+                <Image src="/icons/navbar/scheduling.svg" alt="Scheduling" width={24} height={24} />
+            </Link>
+            <Link href="/ai" className="p-0 bg-primary text-white rounded-xl w-12 h-12 flex items-center justify-center">
+                <Image src="/icons/navbar/ai.svg" alt="AI" width={24} height={24} />
+            </Link>
+            <Link href="/context">
+                <Image src="/icons/navbar/context.svg" alt="Context" width={24} height={24} />
+            </Link>
+            <Link href="/create-post">
+                <Image src="/icons/navbar/plus.svg" alt="Create" width={24} height={24} />
+            </Link>
+        </div>
+    )
+}
