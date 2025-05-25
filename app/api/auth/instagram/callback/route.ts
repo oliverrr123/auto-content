0c3ba9365988c1b1bdab3b09be3f23d160fec522
oliverrr123 <oliver.cingl@gmail.com>
2025-05-25 15:20:17 +0200

    feat: implement instagram login

---
+++ app/api/auth/instagram/callback/route.ts
@@ -0,0 +1,60 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { createClient } from '@/utils/supabase/server';
+
+export async function GET(request: NextRequest) {
+    const { searchParams } = new URL(request.url);
+    const code = searchParams.get('code');
+
+    if (!code) {
+        // TODO
+    }
+
+    // TODO: GET THESE FROM SUPABASE
+    const clientId = process.env.INSTAGRAM_CLIENT_ID;
+    const clientSecret = process.env.INSTAGRAM_CLIENT_SECRET;
+    const redirectUri = process.env.INSTAGRAM_REDIRECT_URI;
+
+    if (!clientId || !clientSecret || !redirectUri) {
+        // TODO
+        return
+    }
+
+    const tokenUrl = 'https://api.instagram.com/oauth/access_token';
+    const params = new URLSearchParams();
+    params.append('client_id', clientId);
+    params.append('client_secret', clientSecret);
+    params.append('grant_type', 'authorization_code');
+    params.append('redirect_uri', redirectUri);
+    params.append('code', code!);
+
+    console.log("CODE")
+    console.log(code);
+    console.log("/CODE")
+
+    try {
+        const response = await fetch(tokenUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: params,
+        });
+
+        const data = await response.json();
+
+        if (!response.ok || data.error_type) {
+            // TODO
+        }
+
+        const accessToken = data.access_token;
+        const instagramUserId = data.user_id;
+
+        console.log(data);
+
+        return NextResponse.redirect(new URL('/context', request.url));
+
+        
+    } catch (error) {
+        // TODO
+    }
+}
