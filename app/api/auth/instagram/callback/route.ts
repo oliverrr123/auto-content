a0e489cb1d75c706fea26d40cfb135d5e5e40d6d
oliverrr123 <oliver.cingl@gmail.com>
2025-05-28 21:30:40 +0200

    fix: error defined but never used

--- app/api/auth/instagram/callback/route.ts
+++ app/api/auth/instagram/callback/route.ts
@@ -66,6 +66,7 @@ export async function GET(request: NextRequest) {
 
         } catch (error) {
             // TODO
+            console.error(error);
         }
 
         return NextResponse.redirect(new URL('/context', request.url));
