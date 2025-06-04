37c5e28250ba9f345a63bd00825ba18ce7cd8ce8
oliverrr123 <oliver.cingl@gmail.com>
2025-06-04 15:26:52 +0200

    fix

--- middleware.ts
+++ middleware.ts
@@ -19,16 +19,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - auth/* (authentication routes)
-     * Feel free to modify this pattern to include more paths.
-     */
-    '/((?!api|auth|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
+  matcher: []
 }
