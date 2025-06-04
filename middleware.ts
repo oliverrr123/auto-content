93435f2589b09e8be01b090cc1041b9fed14bdf8
oliverrr123 <oliver.cingl@gmail.com>
2025-06-04 15:38:08 +0200

    fix

--- middleware.ts
+++ middleware.ts
@@ -19,9 +19,15 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: []
-
-
-
-  
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - auth/confirm (auth confirmation route)
+     */
+    '/((?!api|auth/confirm|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'
+  ]
 }
