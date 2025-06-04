58e686b8e3af08ee7908602f6ed114b966fb9d33
oliverrr123 <oliver.cingl@gmail.com>
2025-06-04 17:30:03 +0200

    fix: build error

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,22 +1,4 @@
-import { useAuth } from "@/context/AuthContext";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
-
 export default function ErrorPage() {
-    const { user, isLoading } = useAuth();
-
-    const router = useRouter();
-
-    useEffect(() => {
-        if (!user && !isLoading) {
-            router.push('/login');
-        }
-    }, [user, isLoading, router]);
-
-    if (!user) {
-        return null;
-    }
-
     return (
         <div>ErrorPage</div>
     )
