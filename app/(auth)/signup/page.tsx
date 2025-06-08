e1b970a9c5b39d81025ac102a17a8bcfd363fa33
oliverrr123 <oliver.cingl@gmail.com>
2025-06-08 13:39:18 +0200

    fix: build error

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,4 +1,3 @@
-import { GalleryVerticalEnd } from "lucide-react"
 import Image from "next/image"
 import { SignupForm } from "@/components/signup-form"
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import MobileNavbar from "@/components/mobile-navbar";
-import MobileHeader from "@/components/mobile-header";
 import { AuthProvider } from "@/context/AuthContext";
 
 const geistSans = Geist({
