b06d7267d0bd07e18935cc9162dc94cc2c392cd0
oliverrr123 <oliver.cingl@gmail.com>
2025-06-08 19:50:11 +0200

    perf: improve navbar performance

--- components/mobile-navbar.tsx
+++ components/mobile-navbar.tsx
@@ -1,29 +1,31 @@
 "use client";
+
 import Link from "next/link";
+import { useState } from "react";
 import { usePathname } from "next/navigation";
 
 export default function MobileNavbar() {
+
     const pathname = usePathname();
-    const isActive = (path: string) => pathname === path;
+    const [active, setActive] = useState(pathname.split("/")[1]);
 
     return (
         <div className="fixed flex justify-between items-center w-full h-12 px-10 bottom-0 left-0 right-0 bg-white drop-shadow-sexy z-50">
             <Link href="/">
-                <img src={isActive("/") ? "/icons/navbar/2/home.svg" : "/icons/navbar/home.svg"} alt="Home" width={24} height={24} />
+                <img src={active === "home" ? "/icons/navbar/2/home.svg" : "/icons/navbar/home.svg"} onClick={() => setActive("home")} alt="Home" width={24} height={24} />
             </Link>
             <Link href="/context">
-                <img src={isActive("/context") ? "/icons/navbar/2/context.svg" : "/icons/navbar/context.svg"} alt="Context" width={24} height={24} />
+                <img src={active === "context" ? "/icons/navbar/2/context.svg" : "/icons/navbar/context.svg"} onClick={() => setActive("context")} alt="Context" width={24} height={24} />
             </Link>
             <Link href="/create-post">
-                <img src={isActive("/create-post") ? "/icons/navbar/2/plus.svg" : "/icons/navbar/plus.svg"} alt="Create" width={24} height={24} />
+                <img src={active === "create-post" ? "/icons/navbar/2/plus.svg" : "/icons/navbar/plus.svg"} onClick={() => setActive("create-post")} alt="Create" width={24} height={24} />
             </Link>
             <Link href="/scheduling">
-                <img src={isActive("/scheduling") ? "/icons/navbar/2/scheduling.svg" : "/icons/navbar/scheduling.svg"} alt="Scheduling" width={24} height={24} />
+                <img src={active === "scheduling" ? "/icons/navbar/2/scheduling.svg" : "/icons/navbar/scheduling.svg"} onClick={() => setActive("scheduling")} alt="Scheduling" width={24} height={24} />
             </Link>
             <Link href="/ai">
-                <img src={isActive("/ai") ? "/icons/navbar/2/ai.svg" : "/icons/navbar/ai.svg"} alt="AI" width={20} height={20} />
+                <img src={active === "ai" ? "/icons/navbar/2/ai.svg" : "/icons/navbar/ai.svg"} onClick={() => setActive("ai")} alt="AI" width={20} height={20} />
             </Link>
         </div>
     )
-
 }
