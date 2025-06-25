840196fb1f2dc6d35466268cc7fbc1073b32aa93
oliverrr123 <oliver.cingl@gmail.com>
2025-06-25 23:07:27 +0200

    feat: better logout

--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { useAuth } from '@/context/AuthContext';
 import Image from "next/image";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { LogOut } from "lucide-react";
+import { LogOut, User } from "lucide-react";
 import { logout } from "@/lib/auth-actions";
 import { Skeleton } from "./ui/skeleton";
 
@@ -23,14 +23,22 @@ export default function MobileNavbar() {
                 {user ? (
                     <Popover>
                         <PopoverTrigger className="text-sm text-slate-600 sm:inline flex items-center gap-2">
-                            {user.user_metadata.avatar_url && (
-                                <Image src={user.user_metadata.avatar_url} alt="User" width={24} height={24} unoptimized priority className="rounded-full" />
-                            )}
-                            <p>{user.user_metadata.full_name || user.email}</p>
+                            <div className="bg-slate-100 p-2 rounded-full">
+                                <User className="w-4 h-4 text-slate-400" />
+                            </div>
                         </PopoverTrigger>
-                        <PopoverContent className="w-32 flex items-center gap-2" onClick={() => logout()}>
-                            <LogOut className="w-4 h-4" />
-                            <p className="text-sm text-slate-600">Logout</p>
+                        <PopoverContent className="w-48 flex flex-col gap-2" onClick={() => logout()}>
+                            <div className="flex items-center gap-2">
+                                {user.user_metadata.avatar_url && (
+                                    <Image src={user.user_metadata.avatar_url} alt="User" width={24} height={24} unoptimized priority className="rounded-full" />
+                                )}
+                                <p>{user.user_metadata.full_name || user.email}</p>
+                            </div>
+                            <hr className="my-2 w-full" /> 
+                            <div className="flex items-center gap-2">
+                                <LogOut className="w-4 h-4" />
+                                <p className="text-sm text-slate-600">Logout</p>
+                            </div>
                         </PopoverContent>
                     </Popover>
                 ) : (
