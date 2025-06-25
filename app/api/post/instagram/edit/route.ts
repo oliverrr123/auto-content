553c39dae483cbb563e00f66c87862de5b369d41
oliverrr123 <oliver.cingl@gmail.com>
2025-06-25 15:44:06 +0200

    fix: build error

--- app/api/post/instagram/edit/route.ts
+++ app/api/post/instagram/edit/route.ts
@@ -1,27 +1,6 @@
 import { NextResponse, NextRequest } from "next/server";
 import { createClient } from "@/utils/supabase/server";
 
-interface Post {
-    id: string;
-    user_id: string;
-    platform: string;
-    params: Array<{
-        filetype: string;
-        isUploading: boolean;
-        taggedPeople: Array<{
-            x: number;
-            y: number;
-            username: string;
-        }>;
-        signedReadUrl: string;
-    }>;
-    schedule_params: {
-        status: string;
-        scheduled_date: string;
-    };
-    caption: string;
-}
-
 export async function POST(request: NextRequest) {
     const { postData } = await request.json();
 
